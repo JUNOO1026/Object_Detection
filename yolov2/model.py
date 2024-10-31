531d264cb05da8a046c51f9c6806ceac0c1633f5
JUNOO1026 <joon10266@gmail.com>
2024-11-01 01:54:47 +0900

    yolov2 model modify

--- yolov2/model.py
+++ yolov2/model.py
@@ -10,6 +10,16 @@ def load_config(path):
 
     return config
 
+def load_model(path):
+    with open(path, 'r') as f:
+        model = yaml.safe_load(f)
+
+    first_half = model['YoloV2_first']
+    second_half = model['YoloV2_second']
+    fcn_layers_channels = model['FCN_layers_channels']
+
+    return first_half, second_half, fcn_layers_channels
+
 class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, bias=False, **kwargs):
         super().__init__()
@@ -26,29 +36,59 @@ class YoloV2(nn.Module):
     def __init__(self, in_channels=3, **kwargs):
         super().__init__()
 
-        self.model_architecture = load_config(path)
+        self.model_first, self.model_second, self.fcn_layers_channels = load_model(path)
         self.in_channels = in_channels
-        self.darknet19 = self._create_block(self.model_architecture)
-        self.fcs = self._create_fcs
+        self.conv1 = self._create_block(self.model_first, self.in_channels)
+        self.conv2 = self._create_block(self.model_second, 512)
+        self.fcs = self._create_fcs(self.fcn_layers_channels)
 
 
     def forward(self, x):
-        x = self.darknet19(x)
-        return self.fcs(torch.flatten(x), start_dim=1)
+        x = self.conv1(x)
+        print(x.shape)
+        _, _, height, width = x.size()
 
-    def _create_block(self, architecture):
+        part1 = x[..., :height // 2, :width // 2] # (BATCH_SIZE, 512, 13, 13)
+        print('part1 : ', part1.shape)
+        part2 = x[..., :height // 2, width // 2:] # (BATCH_SIZE, 512, 13, 13)
+        part3 = x[..., height // 2:, :width // 2] # (BATCH_SIZE, 512, 13, 13)
+        part4 = x[..., height // 2:, width // 2:] # (BATCH_SIZE, 512, 13, 13)
+        residual = torch.cat((part1, part2, part3, part4), dim=1) # (BATCH_SIZE, 2048, 13, 13)
+        print(residual.shape)
+
+        x = torch.cat((self.conv2(x), residual), dim=1)
+        print(x.shape)
+        x = self.fcs(x)
+        print(x.shape)
+        return
+
+    def _create_block(self, model, in_channels):
         layers = []
-        in_channels = self.in_channels
 
-        for x in architecture:
+        for x in model:
             if isinstance(x, list):
                 layers += [CNNBlock(in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3])]
                 in_channels = x[1]
 
             elif isinstance(x, str):
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-
-
         return nn.Sequential(*layers)
 
-    def _create_fcs(self, split_size=7, ):
+
+    def _create_fcs(self, in_channels, num_of_anchor=5, num_of_classes=3):
+        layers = []
+        out_channels = num_of_anchor * (num_of_classes + 5)
+
+        layers = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+
+        final_layers = nn.Sequential(layers)
+        final_out = final_layers.permute(0, 2, 3, 1).contiguous()
+
+        return final_out.view(final_out(0), final_out(1), final_out(2), num_of_anchor, num_of_classes + 5)
+
+
+
+
+x = torch.randn(4, 3, 416, 416)
+model = YoloV2()
+print(model(x))
