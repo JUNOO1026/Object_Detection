662b4a4e82b6b05d519c5a74f8c423e2c9283720
JUNOO1026 <joon10266@gmail.com>
2024-11-13 17:57:17 +0900

    MobileNetv1 architecture fin

--- RetinaFace/model/resnet_50.py
+++ RetinaFace/model/resnet_50.py
@@ -1,110 +1,103 @@
 import torch
 from torch import nn
-
-
-class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
-        super().__init__()
-
-        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.act = nn.ReLU()
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-    
-
-# class ResidualBlock(nn.Module):
-#     def __init__(self, in_channels, out_channels, num_repeat, projection, **kwargs):
-#         super().__init__()
-        
-#         self.layers = nn.ModuleList([
-#             nn.Sequential(
-#                 CNNBlock(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-#                 nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-#                 nn.BatchNorm2d(out_channels)
-#             ) 
-#             for _ in range(num_repeat)
-#         ])
-#         self.projection = projection
-#         self.act = nn.ReLU()
-
-#     def forward(self, x):
-#         for layer in self.layers:
-#             x = layer(x) # element-wise
-
-#             if self.projection is not None:
-#                 shorcut = self.projection(x)
-
-#             else:
-#                 shortcut = x
-            
-#             output = self.act(x + shortcut)
-
-#         return output
-    
+from torchinfo import summary
 
 class BottleNeck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_channels, out_channels, num_repeat, projection, **kwargs):
+    def __init__(self, in_channels, out_channels, stride=1, projection=None, **kwargs):
         super().__init__()
 
-        self.layers = nn.ModuleList([
-            nn.Sequential(
-                CNNBlock(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
-                CNNBlock(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0),
-                nn.BatchNorm2d(out_channels * self.expansion)
-            )
-            for _ in range(num_repeat)
-        ])
-        
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels * self.expansion)
+        )
+
         self.projection = projection
         self.act = nn.ReLU()
 
     def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        residual = self.residual(x)
 
-            if self.projection is not None:
-                shortcut = self.projection(x)
-            else:
-                shortcut = x
+        if self.projection is not None:
+            shortcut = self.projection(x)
+        else:
+            shortcut = x
 
-        output = self.act(x + shortcut)
+        output = self.act(residual + shortcut)
 
         return output
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, block_list, num_classes=1000, zero_init_residual=True, **kwargs):
+        super().__init__()
+
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3)
+        self.batch1 = nn.BatchNorm2d(64)
+        self.act1 = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.stage1 = self.make_layers(block, 64, block_list[0], stride=1)
+        self.stage2 = self.make_layers(block, 128, block_list[1], stride=2)
+        self.stage3 = self.make_layers(block, 256, block_list[2], stride=2)
+        self.stage4 = self.make_layers(block, 512, block_list[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity='relu')
+
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, block):
+                    nn.init.constant_(m.residual[-1].weight, 0)
+
+    def make_layers(self, block, out_channels, num_block, stride=1, **kwargs):
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            projection = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion))
+        else:
+            projection = None
+
+        layers = []
+        layers += [block(self.in_channels, out_channels, stride, projection)]
+
+        self.in_channels = out_channels * block.expansion
+
+        for _ in range(1, num_block):
+            layers += [block(self.in_channels, out_channels)]
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batch1(x)
+        x = self.act1(x)
+        x = self.maxpool(x)
+        x = self.stage1(x)
+        print(x.shape)
+        x = self.stage2(x)
+        print(x.shape)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
     
 
-    class ResNet(nn.Module):
-        def __init__(self, in_channels, block, **kwargs):
-            super().__init__()
-
-            self.block = block # [3, 4, 6, 3]
-
-            self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3)
-            self.bn1 = nn.BatchNorm2d(64)
-            self.act1 = nn.ReLU()
-            self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-            
-            self.in_channels = 64
-
-            self.module = self._create_block(self.block)
+def resnet50(**kwargs):
+    return ResNet(BottleNeck, [3, 4, 6, 3], **kwargs)
 
 
-        def forward(self, x):
-            pass
-
-        def _create_block(self, block):
-            in_channels = self.in_channels
-            layers = []
-
-
-
-
-
-
-
-    
+model = resnet50()
+summary(model, input_size=(2, 3, 224, 224), device='cuda')
---
+++ landmark/mobilenet_v1.py
@@ -0,0 +1,124 @@
+import os
+import yaml
+import torch
+from torch import nn
+from torchinfo import summary
+
+# print(os.getcwd())
+
+path = './landmark/config/mobilenetV1.yaml'
+
+
+def load_model(path):
+    with  open(path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    model = config['model_architecture']
+
+    return model
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+
+class DepthWiseBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+
+        self.DW_conv = CNNBlock(in_channels, 
+                                out_channels, 
+                                kernel_size=3, 
+                                groups=in_channels,
+                                padding=1,
+        )
+        
+    def forward(self, x):
+        return self.DW_conv(x)
+    
+
+class PointWiseBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+
+        self.PW_conv = CNNBlock(in_channels,
+                                out_channels,
+                                kernel_size=1,
+                                stride=1,
+                                padding=0,
+        )
+
+    def forward(self, x):
+        return self.PW_conv(x)
+    
+
+class MobileNetV1(nn.Module):
+    def __init__(self, in_channels, num_classes, **kwargs):
+        super().__init__()
+
+        self.architecture = load_model(path)
+        self.in_channels = 32
+        self.layers = self._create_block(self.architecture)
+ 
+        
+        self.f_conv = nn.Conv2d(in_channels, 32, kernel_size=3, stride=2, padding=1)
+        self.bn = nn.BatchNorm2d(32)
+        self.act = nn.ReLU()
+        self.l_conv = nn.Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.act(self.bn(self.f_conv(x)))
+        x = self.layers(x)
+        x = self.l_conv(x)
+        x = self.pool(x)
+        print("x.shape : ", x.shape)
+        x = x.view(x.size(0), -1)
+        print("x.shape : ", x.shape)
+        x = self.fc(x)
+
+        return x
+
+    def _create_block(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if isinstance(x, list) and len(x) == 2:
+                layer1 = x[0]
+                layer2 = x[1]
+                layers += [
+                    DepthWiseBlock(in_channels, layer1[0], kernel_size=layer1[1], stride=layer1[2]),
+                    PointWiseBlock(layer1[0], layer2[0], kernel_size=layer2[1], stride=layer2[2]),
+                ]
+                in_channels = layer2[0]
+
+            elif isinstance(x, list) and len(x) == 3:
+                num_repeat = x[0]
+                layer1 = x[1]
+                layer2 = x[2]
+                
+                for _ in range(num_repeat):
+                    layers += [
+                        DepthWiseBlock(in_channels, layer1[0], kernel_size=layer1[1], stride=layer1[2]),
+                        PointWiseBlock(layer1[0], layer2[0], kernel_size=layer2[1], stride=layer2[2])
+                    ]
+                    in_channels = layer2[0]
+
+
+        return nn.Sequential(*layers)
+    
+
+a = torch.randn(1, 3, 224, 224)
+
+model = MobileNetV1(in_channels=3, num_classes=1000)
+print(model(a).shape)
